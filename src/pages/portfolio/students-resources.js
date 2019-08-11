b9842744ac487db1a3a21722dd63610427fbc098
celinechole <github@celinechole.com>
2019-08-11 16:48:58 -0400

    add students resources

---
+++ src/pages/portfolio/students-resources.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import Layout from '../../components/Layout';
+
+const IndexPage = () => (
+  <Layout fullMenu>
+    <article id="main">
+      <header>
+        <h2>Students Resources</h2>
+        <p>Sample of resources built for students</p>
+      </header>
+      <section className="wrapper style5">
+        <div className="inner">
+          <h3>10 days of JavaScript</h3>
+          <p>
+            This is a collection of 10 JavaScript problem I created to help
+            students consolidate their knowledge.
+          </p>
+          <a
+            href="https://gist.github.com/CelineChole/fed4df894f83f02fa750bd92cddded74"
+            target="blank"
+          >
+            Link to the gist.
+          </a>
+
+          <hr />
+
+          <h3>Recursion</h3>
+          <p>
+            I lead a review lesson on recursion -{' '}
+            <a
+              href="https://gist.github.com/CelineChole/1fa7d405b1efeed3ebfe04047c6b8342"
+              target="blank"
+            >
+              link to the gist.
+            </a>
+          </p>
+
+        </div>
+      </section>
+    </article>
+  </Layout>
+);
+
+export default IndexPage;
